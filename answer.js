277f3022642e3848043850908c65a810725e34ef
Dawid Paśko <dawpasko@gmail.com>
2023-11-01 11:48:02 +0100

    eye animation

---
+++ answer.js
@@ -0,0 +1,9 @@
+const geoAnswers = [
+  "India	1,425.8 M, China	1,425.7 M, United States	333.3 M, Indonesia	275.5 M, Pakistan	235.8 M, Nigeria	218.5 M, Brazil	215.3 M, Bangladesh	171.2 M, Russia	143.6 M, Mexico	127.5 M	",
+];
+
+const geoQuestions = (geoAnswers) => {
+  const question = document.getElementById("questionInput")
+
+  console.log(question)
+};
--- app.js
+++ app.js
@@ -1,38 +1,28 @@
-// SCROLL DOWN ANIMATION
-window.addEventListener("scroll", () => {
-  
+const animateScrollDown = () => {
   const element = document.getElementById("animation");
-  const footer = document.getElementById("footerSection")
-  const Logo = document.getElementById("Logo")
+  const footer = document.getElementById("footerSection");
+  const Logo = document.getElementById("Logo");
+
+  element.style.marginTop = "-560px";
+  footer.style.marginBottom = "0";
+  Logo.style.marginTop = "16px";
+};
+
+const animateQuestionSection = () => {
+  const element = document.getElementById("secondSection");
   
-  const scrollable = window.scrollY;
-  const scrolled = 120;
+  element.style.opacity = 1;
+};
+
+const animationChange = () => {
+  const iris = document.getElementById("iris");
   
-  if (scrolled <= scrollable){
-    element.style.marginTop = "-500px";
-    footer.style.marginBottom = "0";
-    Logo.style.marginTop = "16px"
-  }
+  iris.style.animation = " move-eye-skew2 5s ease-out infinite";
+}
 
-})
-
-
-
-// QUESTION SECTION ANIMATION 
-
-
-
-
-window.addEventListener("scroll", () => {
-  const element = document.getElementById("#secondSection");
-
-  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
-  const scrolled = window.scrollY;;
-  
-  if (scrolled === scrollable){
-    element.style.animation = delete 20 ;
-    element.style.opacity = 1 ;
-  }
-
-})
+window.addEventListener("wheel", () => {
+  animateQuestionSection();
+  animateScrollDown();
+  animationChange();
+});
 
