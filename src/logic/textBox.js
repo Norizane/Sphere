884c6abaa09710334b0c2b425692baf4801e6c54
Dawid Paśko <dawpasko@gmail.com>
2023-11-23 21:02:57 +0100

    feat/sphere-logic: localStorage update init

--- src/logic/textBox.js
+++ src/logic/textBox.js
@@ -1,15 +1,8 @@
-import { facts, jokes, answers } from './textBoxData';
-
-
-const getStoredData = (key) => {
-  const storedData = localStorage.getItem(key);
-  return storedData ? JSON.parse(storedData) : null;
-};
-
-
-const setStoredData = (key, data) => {
-  localStorage.setItem(key, JSON.stringify(data));
-};
+import { facts, jokes, answers } from "./textBoxData";
+import {
+  addData,
+  displayStoredData,
+} from "./localStorage";
 
 const getTextBoxData = (data) => {
   const randomNumber = Math.floor(Math.random() * data.length);
@@ -19,12 +12,11 @@ const getTextBoxData = (data) => {
 const displayTextBox = (text) => {
   const Paragraph = document.createElement("p");
   const answerOutput = document.getElementById("answerOutput");
- 
+
   Paragraph.innerHTML = text;
 
   Paragraph.classList.add("textBox");
   answerOutput.appendChild(Paragraph);
- 
 };
 
 const showAnswerForQuestion = (question, answer) => {
@@ -32,33 +24,25 @@ const showAnswerForQuestion = (question, answer) => {
   const questionParagraph = document.createElement("p");
   questionParagraph.textContent = question;
   questionParagraph.classList.add("question-text");
-  
 
   const answerParagraph = document.createElement("p");
   answerParagraph.textContent = answer;
   answerParagraph.classList.add("textBox");
- 
+
   answerOutput.appendChild(questionParagraph);
   answerOutput.appendChild(answerParagraph);
-
-  const storedData = getStoredData("qaData") || [];
-  storedData.push({ question, answer });
-  setStoredData("qaData", storedData);
-  
 };
 
-
-
-
-const answerOutput = () => {
+const questionOutput = () => {
   const questionInput = document.getElementById("questionInput");
   const questionText = questionInput.value;
   questionInput.value = "";
 
   const randomAnswer = getTextBoxData(answers);
   showAnswerForQuestion(questionText, randomAnswer);
+  addData("Question", questionText);
+  addData("Answer", randomAnswer);
 };
-
 
 export const initTextBoxBehavior = () => {
   const jokeButton = document.getElementById("jokeButton");
@@ -68,49 +52,28 @@ export const initTextBoxBehavior = () => {
   jokeButton.addEventListener("click", () => {
     const randomJoke = getTextBoxData(jokes);
     displayTextBox(randomJoke);
-
-    const storedJokes = getStoredData("jokesData") || [];
-    storedJokes.push(randomJoke);
-    setStoredData("jokesData", storedJokes);
+    addData("Joke", randomJoke);
   });
 
   factButton.addEventListener("click", () => {
     const randomFact = getTextBoxData(facts);
     displayTextBox(randomFact);
-  
-    const storedFacts = getStoredData("factsData") || [];
-    storedFacts.push(randomFact);
-    setStoredData("factsData", storedFacts);
+    addData("Fact", randomFact);
   });
 
   window.addEventListener("keyup", (event) => {
     if (event.key === "Enter") {
-      answerOutput();
+      questionOutput();
     }
   });
 
-  const storedQuestionsAndAnswers = getStoredData("qaData") || [];
-  storedQuestionsAndAnswers.forEach(({ question, answer }) => {
-    showAnswerForQuestion(question, answer);
-  });
-
-  const storedJokes = getStoredData("jokesData") || [];
-  storedJokes.forEach((joke) => {
-    displayTextBox(joke);
-  });
-
-  const storedFacts = getStoredData("factsData") || [];
-  storedFacts.forEach((fact) => {
-    displayTextBox(fact);
-  });
-  // TODO:potential scroll down button
   scrollDownButton.addEventListener("click", () => {
     const container = document.getElementById("questionContainer");
-  
     container.scrollBy({
-      top: 100000000, 
+      top: 100000000,
       behavior: "smooth",
     });
   });
 
-  }
+  displayStoredData();
+};
